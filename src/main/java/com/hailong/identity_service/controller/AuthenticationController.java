c33ca38a8e5af12a82eb7a04f873e5cb783ac2bd
LongBii24 <biiboy2k3@gmail.com>
2026-06-25 23:54:18 +0700

    bcypto and jwt

---
+++ src/main/java/com/hailong/identity_service/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package com.hailong.identity_service.controller;
+
+import com.hailong.identity_service.dto.request.AuthenticationRequest;
+import com.hailong.identity_service.dto.request.IntrospectRequest;
+import com.hailong.identity_service.dto.response.ApiResponse;
+import com.hailong.identity_service.dto.response.AuthenticationResponse;
+import com.hailong.identity_service.dto.response.IntrospectResponse;
+import com.hailong.identity_service.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/log-in")
+    ApiResponse<AuthenticationResponse> autheticated(@RequestBody AuthenticationRequest request){
+        var result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> introspected(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
+                .build();
+    }
+}
